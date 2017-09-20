22f260a1d5898076a4657dbc6234a9fe77e97f10
Rick <givemepassxd999@gmail.com>
2017-09-20 19:29:06 +0800

    加入drawerlayout sample

--- app/src/main/java/com/example/givemepass/drawerlayoutdemo/MainActivity.java
+++ app/src/main/java/com/example/givemepass/drawerlayoutdemo/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.givemepass.drawerlayoutdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
+        setSupportActionBar(toolbar);
     }
 }
